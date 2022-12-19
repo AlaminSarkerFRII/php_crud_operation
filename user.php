c30121d3781caa6d8dab5b7227aebefb0feb8aae
AlaminSarkerFRII <alamin.education1997@gmail.com>
2022-12-19 23:55:20 +0600

    setup

---
+++ user.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Crud Operations</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+</head>
+
+<body>
+
+    <form>
+        <div class="row w-50 mx-auto my-4">
+            <div class="col-md-6 mb-3">
+                <label class="form-label">Name</label>
+                <input type="email" class="form-control" aria-describedby="emailHelp">
+
+            </div>
+            <div class="col-md-6 mb-3">
+                <label class="form-label">Email address</label>
+                <input type="email" class="form-control" aria-describedby="emailHelp">
+
+            </div>
+            <div class="col-md-6 mb-3">
+                <label class="form-label">Password</label>
+                <input type="password" class="form-control">
+            </div>
+            <div class="col-md-6 mb-3">
+                <label class="form-label">mobile</label>
+                <input type="number" class="form-control">
+            </div>
+
+            <button class="w-50 mx-auto bg-primary text-white p-2" type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </form>
+
+</body>
+
+</html>
